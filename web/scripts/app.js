e464ce21b0999715eda3bd738edfc82c0c56d6b6
= <craig@craigshoemaker.net>
2018-04-30 13:24:47 -0400

    Refactored web client code - Add Handlebars templating engine - Move templates into markup (out of script) - Add favicon - Modernize JavaScript syntax - Add helper functions to make code more readable

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,178 +1,210 @@
-// Upload videos/images to Blob storage
-$('#uploadFile').on('change', function () {
-  // Update value
-  $('#uploadResult').html('Uploading ...')
+window.viewModel = {};
 
-  var file = $('#uploadFile').prop('files')[0]
-  console.log(file)
+$(function(){
 
-  // Image or video upload ?
-  var fileExtension = file.name.split('.').pop()
-  var blobPrefix = 'images/'
-  var isImage = /jpe?g|png|gif/.test(fileExtension)
-  var isVideo = /mpe?g|mp4/.test(fileExtension)
-
-  if (isImage || isVideo) {
-    blobPrefix = isImage ? 'images/' : 'videos/'
-  } else {
-    alert('File format not supported')
-    $('#uploadResult').html('Upload a new file')
-    return
+  const config = {
+    blobUri: 'https://build2018demo.blob.core.windows.net/',
+    sasToken: YOUR_SAS_TOKEN_HERE,
+    loc: window.location.pathname,
+    container:  'web', // config.loc.substring(1, config.loc.lastIndexOf('/')); //change to this when in blob storage account
   }
-
-  // Upload the file, and update the screen
-  blobService.createBlockBlobFromBrowserFile(container, blobPrefix + file.name, file, null, function (error, result, response) {
-    if (error) {
-      alert(error)
-    } else {
-      $('#uploadResult').html('Done!')
-      setTimeout(function () {
-        $('#uploadResult').html('Upload a new file!')
-        listBlobs('images')
-        listBlobs('videos')
-      }, 2000)
+  
+  const blobService = AzureStorage.Blob.createBlobServiceWithSas(config.blobUri, config.sasToken)
+  
+  const getBlobPrefix = fileName => {
+    const extension = fileName.split('.').pop()
+    const isImage = isImageExtension(extension)
+    const isVideo = isVideoExtension(extension)
+  
+    let prefix = ''
+  
+    if (isImage || isVideo) {
+      prefix = isImage ? 'images/' : 'videos/'
     }
-  })
-})
-
-$('#viewMedia').on('show.bs.modal', function (e) {
-  var source = $(e.relatedTarget).attr('data-src')
-  var extension = source.split('.').pop()
-  if (/mpe?g|mp4/.test(extension)) {
-    $('#video').remove()
-    var video = $('<video />', {
-      id: 'video',
-      controls: true,
-      width: '100%'
-    })
-    var videosource = $('<source />', {
-      src: source,
-      type: 'video/mp4'
-    })
-    videosource.appendTo(video)
-    var enTrack = $('<track />', {
-      id: 'en-track',
-      src: (source.replace('transcoded/', 'subtitles/en-US/') + '.vtt'),
-      label: 'English',
-      kind: 'subtitles',
-      srclang: 'en'
-    })
-    var jaTrack = $('<track />', {
-      id: 'ja-track',
-      src: (source.replace('transcoded/', 'subtitles/ja-JP/') + '.vtt'),
-      label: 'Japanese',
-      kind: 'subtitles',
-      srclang: 'ja'
-    })
-    enTrack.appendTo(video)
-    jaTrack.appendTo(video)
-    video.appendTo($('#media'))
-
-    $('.img-responsive').removeAttr('src')
-  } else {
-    $('#video').remove()
-    $('.img-responsive').attr('src', source)
+  
+    return prefix
   }
-})
 
-$('#viewMedia').on('hide.bs.modal', function (e) {
-  if ($('video')[0]) {
-    $('video')[0].pause()
-  }
-  $('.img-responsive').removeAttr('src')
-  $('.video-responsive').removeAttr('src')
-  $('.video-responsive').prop('hidden', true)
-})
+  viewModel = {
+    listBlobs: prefix => {
 
-// List blobs in the given prefix
-function listBlobs (prefix) {
-  blobService.listBlobsSegmentedWithPrefix(container, prefix, null, {
-    include: 'metadata'
-  }, function (error, results) {
-    if (error) {
-      console.log('Failed to list objects')
-    } else {
-      var listResult = ''
-      for (var i = 0, blob; blob = results.entries[i]; i++) {
-        var metadata = ''
-        metadata = blob.metadata.caption ? blob.metadata.caption : 'Processing...'
-        var thumbUri, fullUri, duration = ''
-        if (prefix === 'videos') {
-          thumbUri = blobUri + container + '/thumbnails/' + blob.name.split('/').pop() + '.jpg'
-          fullUri = blobUri + container + '/transcoded/' + blob.name.split('/').pop()
-          duration = blob.metadata.duration ? blob.metadata.duration + ' sec' : ''
+      $('#' + prefix).html('')
+  
+      blobService.listBlobsSegmentedWithPrefix(config.container, prefix, null, {
+        include: 'metadata'
+      }, (error, results) => {
+        if (error) {
+          console.log('Failed to list objects')
         } else {
-          thumbUri = blobUri + container + '/' + blob.name
-          fullUri = thumbUri
+          let listResult = ''
+  
+          results.entries.forEach(blob => {
+  
+            const context = {
+              metadata: blob.metadata.caption ? blob.metadata.caption : 'Processing...',
+              duration: '',
+              blobName: blob.name,
+              prefix: prefix
+            }
+  
+            if (prefix === 'videos') {
+              context.thumbUri = getVideoThumbnailURI(blob.name)
+              context.fullUri = getVideoFullURI(blob.name)
+              context.duration = getVideoDuration(blob.metadata.duration)
+            } else {
+              context.thumbUri = getImageThumbnailURI(blob.name)
+              context.fullUri = context.thumbUri
+            }
+  
+            const template = Handlebars.compile($blobItemTemplate.html())
+            $('#' + prefix).append(template(context))
+          })
+  
+          Holder.run({
+            images: '.placeholder'
+          })
         }
-        listResult += '<div class="col-md-4"><div class="card mb-4 box-shadow"><img class="card-img-top placeholder" alt="' + metadata + '" src="' + thumbUri + '" onerror="this.src=\'style/no-image.jpg\'"><div class="card-body"><p class="card-text">' + metadata + '</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><button type="button" class="btn btn-primary"  data-toggle="modal" data-target="#viewMedia" data-src="' + fullUri + '">View</button><button type="button" class="btn btn-sm btn-outline-secondary" onClick="deleteBlob(\'' + blob.name + '\', \'' + prefix + '\');">Delete</button></div><i>' + duration + '</i></div></div></div></div>'
-      }
-      grid = $('#' + prefix)
-      grid.html(listResult)
-      Holder.run({
-        images: '.placeholder'
+      })
+    },
+
+    deleteBlob: (blobName, prefix) => {
+      blobService.deleteBlob(config.container, blobName, (error, results) => {
+        if (error) {
+          alert('Failed to delete the object ' + blobName)
+        } else {
+          console.log(blobName + ' deleted')
+          viewModel.listBlobs(prefix)
+        }
       })
     }
-  })
-}
+  }
+  
+  const isVideoExtension = (extension) => /mpe?g|mp4/.test(extension)
+  
+  const isImageExtension = (extension) => /jpe?g|png|gif/.test(extension)
+  
+  const isFileFormatSupported = (prefix) => prefix.length > 0
+  
+  const uploadFile = (prefix, file, callback) => blobService.createBlockBlobFromBrowserFile(config.container, prefix + file.name, file, null, callback)
+  
+  const getVideoThumbnailURI = blobName => config.blobUri + config.container + '/thumbnails/' + blobName.split('/').pop() + '.jpg'
+  
+  const getVideoFullURI = blobName => config.blobUri + config.container + '/transcoded/' + blobName.split('/').pop()
+  
+  const getVideoDuration = duration => duration ? duration + ' sec' : ''
+  
+  const getImageThumbnailURI = blobName => config.blobUri + config.container + '/' + blobName
 
-// Delete a given blob
-function deleteBlob (blobName, prefix) {
-  blobService.deleteBlob(container, blobName, function (error, results) {
-    if (error) {
-      alert('Failed to delete the object ' + blobName)
+  $uploadResult = $('#uploadResult');
+  $blobItemTemplate = $('#blobItemTemplate')
+
+  $('#uploadFile').on('change', () => {
+
+    $uploadResult.html('Uploading ...')
+  
+    const file = $('#uploadFile').prop('files')[0]
+    console.log(file)
+
+    const prefix = getBlobPrefix(file.name);
+
+    if(isFileFormatSupported(prefix)){
+
+      uploadFile(prefix, file, (error, result, response) => {
+        if (error) {
+          alert(error)
+        } else {
+          $uploadResult.html('Done!')
+          setTimeout(() => {
+            $uploadResult.html('Upload a new file!')
+            viewModel.listBlobs('images')
+            viewModel.listBlobs('videos')
+          }, 2000)
+        }
+      })
+
     } else {
-      console.log(blobName + ' deleted')
-      listBlobs(prefix)
+      alert('File format not supported')
+      $uploadResult.html('Upload a new file')
     }
   })
-}
 
-// Helper function to update the upload result
-function updateResult (text) {
-  $('#uploadResult').html(text)
-}
+  $('#viewMedia').on('show.bs.modal', (e) => {
+    const source = $(e.relatedTarget).attr('data-src')
+    const extension = source.split('.').pop()
 
-// Connect to Blob Storage
-var blobUri = 'https://build2018demo.blob.core.windows.net/'
-var loc = window.location.pathname
-var container = 'web' // loc.substring(1, loc.lastIndexOf('/')); !change to this when in blob storage account
+    if (isVideoExtension(extension)) {
 
-// Place the SAS token here
-var sasToken = ''
-var blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, sasToken)
-listBlobs('images')
-listBlobs('videos')
+      $('#video').remove()
 
-// Azure Search Code
-// Initialize and connect to your search service
-var automagic = new AzSearch.Automagic({
-  index: 'video-index',
-  queryKey: '879818179156303DCBDFF90E8C4D4CD5',
-  service: 'builddemosearchservice'
+      const video = $('<video />', {
+        id: 'video',
+        controls: true,
+        width: '100%'
+      })
+
+      const videosource = $('<source />', {
+        src: source,
+        type: 'video/mp4'
+      })
+
+      videosource.appendTo(video)
+
+      const enTrack = $('<track />', {
+        id: 'en-track',
+        src: (source.replace('transcoded/', 'subtitles/en-US/') + '.vtt'),
+        label: 'English',
+        kind: 'subtitles',
+        srclang: 'en'
+      })
+
+      const jaTrack = $('<track />', {
+        id: 'ja-track',
+        src: (source.replace('transcoded/', 'subtitles/ja-JP/') + '.vtt'),
+        label: 'Japanese',
+        kind: 'subtitles',
+        srclang: 'ja'
+      })
+
+      enTrack.appendTo(video)
+      jaTrack.appendTo(video)
+      video.appendTo($('#media'))
+  
+      $('.img-responsive').removeAttr('src')
+    } else {
+      $('#video').remove()
+      $('.img-responsive').attr('src', source)
+    }
+  })
+  
+  $('#viewMedia').on('hide.bs.modal', (e) => {
+    if ($('video')[0]) {
+      $('video')[0].pause()
+    }
+    $('.img-responsive').removeAttr('src')
+    $('.video-responsive').removeAttr('src')
+    $('.video-responsive').prop('hidden', true)
+  })
+  
+  viewModel.listBlobs('images')
+  viewModel.listBlobs('videos')
+  
+  // Azure Search Code
+  // Initialize and connect to your search service
+  const automagic = new AzSearch.Automagic({
+    index: 'video-index',
+    queryKey: '879818179156303DCBDFF90E8C4D4CD5',
+    service: 'builddemosearchservice'
+  })
+  
+  const css = {
+    searchResults__result: 'col-md-4'
+  }
+
+  automagic.addResults('results', {
+    count: true
+  }, $('#resultTemplate').html(), css)
+  
+  automagic.addPager('pager')
+  automagic.addSearchBox('searchBox')
+
 })
-
-// add a results view using the template defined above
-var resultTemplate =
-  `<div class="card mb-4 box-shadow">
-    <img class="card-img-top placeholder" alt="{{caption}}" src="thumbnails/{{metadata_storage_name}}.jpg" >
-    <div class="card-body">
-      <p class="card-text">{{caption}}</p>
-      <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#viewMedia" data-src="transcoded/{{metadata_storage_name}}">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary" onClick="deleteBlob('{{metadata_storage_name}}', 'videos');">Delete</button>
-        </div>
-        <i>{{duration}}</i>
-      </div>
-    </div>
-  </div>`
-
-var css = {
-  searchResults__result: 'col-md-4'
-}
-automagic.addResults('results', {
-  count: true
-}, resultTemplate, css)
-automagic.addPager('pager')
-automagic.addSearchBox('searchBox')
